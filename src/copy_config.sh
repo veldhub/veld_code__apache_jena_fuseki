12fe8197b38cb59b62ffcc9bd4291fd6bc8781ff
SteffRhes <steffrhes@pm.me>
2024-12-21 10:01:53 +0100

    veld_run_server.yaml: implement flexible shiro.ini injection

---
+++ src/copy_config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+in_shiro_path=/veld/input/config/shiro.ini
+
+if [ -e "$in_shiro_path" ]; then
+  echo "found ${in_shiro_path}, copying to fuseki folder" 
+  cp "$in_shiro_path" /veld/code/run/shiro.ini
+else
+  echo "did not find ${in_shiro_path}, using default" 
+fi
+
+exec "$@"
+
