34ae44c0d55d144375d0f12af93e18eb8a79bb76
cuda@jupyter <cuda@jupyter.arz.oeaw.ac.at>
2024-12-22 05:06:59 -0500

    veld_export.yaml: extend functionality

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-#./s-query --service http://localhost:3030/mydataset/sparql --query query.rq --output json
-
 function export {
-  command="/veld/code/bin/s-query --service ${fuseki_server_url}:${fuseki_server_port}/${fuseki_dataset_name}/sparql --query ${1} --output ${out_format}"
+  command="/veld/code/bin/s-query --service ${fuseki_server_url}:${fuseki_server_port}/${fuseki_dataset_name}/sparql --query ${1} --output ${out_format} > /veld/output/${out_file}"
   echo "$command"
   eval "$command"
 }
