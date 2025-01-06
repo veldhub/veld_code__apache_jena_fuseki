272e4d51837662e7946ee758af853d83809c3158
SteffRhes <steffrhes@pm.me>
2025-01-06 10:35:55 +0100

    veld_export.yaml: remove custom 'rdf_export' function entirely in favor of executing it all in one script

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-function rdf_export {
-  command="/veld/code/bin/s-query --service ${fuseki_server_url}:${fuseki_server_port}/${fuseki_dataset_name}/sparql --query ${1} --output ${out_format} > /veld/output/${out_file}"
-  echo "$command"
-  eval "$command"
-}
-
-rdf_export /veld/input/"$in_query_file"
+command="/veld/code/bin/s-query --service ${fuseki_server_url}:${fuseki_server_port}/${fuseki_dataset_name}/sparql --query /veld/input/${in_query_file} --output ${out_format} > /veld/output/${out_file}"
+echo "$command"
+eval "$command"
 
