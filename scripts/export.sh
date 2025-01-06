3f0a302c94f51d31b2b05f6189c5f5fa4b4a6668
SteffRhes <steffrhes@pm.me>
2025-01-06 10:32:48 +0100

    veld_export.yaml: rename 'export' bash function to 'rdf_export' to avoid shadowing bash 'export' builtin function

--- scripts/export.sh
+++ scripts/export.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-function export {
+function rdf_export {
   command="/veld/code/bin/s-query --service ${fuseki_server_url}:${fuseki_server_port}/${fuseki_dataset_name}/sparql --query ${1} --output ${out_format} > /veld/output/${out_file}"
   echo "$command"
   eval "$command"
 }
 
-export /veld/input/"$in_query_file"
+rdf_export /veld/input/"$in_query_file"
 
